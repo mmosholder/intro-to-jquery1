9a9c0c05786d89feafd245f33ec4f78f40946833
Malinda Mosholder <m.m.mosholder@gmail.com>
2015-04-09 11:12:20 -0600

    completed part 1 of exercise

--- main.js
+++ main.js
@@ -1,5 +1,14 @@
 $(document).on('ready', function() {
-  $('button').on('click', function(){
-  	console.log("Hey! you clicked a button!");
+  $('.first').on('click', function(){
+  	$('body').append('<h2>Hey i am a header!</h2>');
+  });
+
+  $('.second').on('click', function(){
+  	$('body').append('<h1>Appended h1</h1>');
+  });
+
+  $('.third').on('click', function () {
+  	$('body').append('<ul><li>1</li><li>2</li><li>3</li></ul>');
   })
+
 });
